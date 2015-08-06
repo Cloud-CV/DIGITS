ee3519b5d5c95eeb59b48fe81fa368468db520a6
Deshraj <deshrajdry@gmail.com>
2015-08-06 13:13:50 +0530

    Workspace base view created

---
+++ digits/base_workspace.py
@@ -0,0 +1,14 @@
+from django_import import *
+from digitsdb.models import Job as WorkspaceJob
+from organizations.models import OrganizationUser, Organization
+from urlparse import urlparse, parse_qs
+import base64
+
+def get_workspace_details(url):
+	result = {}
+	workspace_id = parse_qs(urlparse(str(url)).query, keep_blank_values=True)["workspace"][0].encode('utf-8')
+	result['workspace_id'] = base64.b64decode(workspace_id)
+	result['workspace_hash'] = str(workspace_id)
+	result['workspace_name'] = Organization.objects.get(pk = str(result['workspace_id'])).name.encode('utf-8')
+	return result
+
