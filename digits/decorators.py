1a5954be1b47006962722a7b3e578c91283e6b5c
Deshraj <deshrajdry@gmail.com>
2015-08-19 07:45:33 +0530

    Fix: login_required decorator fixed and access_required decorator added

--- digits/decorators.py
+++ digits/decorators.py
@@ -1,29 +1,39 @@
 from django_import import *
 from functools import wraps
 from flask import g, request, redirect, url_for
+from digits.base_workspace import get_user_from_session, get_workspace_details
+from organizations.models import OrganizationUser
 
+import flask
 import redis
 redis_conn = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 def login_required(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        djsession_id = request.cookies.get("sessionid")
-        print djsession_id
-        if djsession_id is None:
-            print "Out from 1st condition"
-            print djsession_id
-            return redirect("http://127.0.0.1:8000/accounts/login", code = 302)
+	@wraps(f)
+	def decorated_function(*args, **kwargs):
+		djsession_id = request.cookies.get("sessionid")
+		if djsession_id is None:
+			return redirect("http://localhost:8000/accounts/login", code = 302)
 
-        # key = get_session_prefixed(djsession_id)
-        key = djsession_id
-        session_store = SessionStore(key)
-        auth = session_store.load()
+		key = djsession_id
+		session_store = SessionStore(key)
+		auth = session_store.load()
 
-        if not auth:
-            return redirect("http://127.0.0.1:8000/accounts/login", code = 302)
+		if not auth:
+			return redirect("http://localhost:8000/accounts/login", code = 302)
 
-        g.user_id = str(auth.get("sessionid"))
-        return f(*args, **kwargs)
-    return decorated_function
+		g.user_id = str(auth.get("sessionid"))
+		return f(*args, **kwargs)
+	return decorated_function
+
+def access_required(f):
+	@wraps(f)
+	def decorated_function(*args, **kwargs):
+		workspace = get_workspace_details(flask.request.url)
+		key = request.cookies.get("sessionid")
+		user = get_user_from_session(key)
+		if not OrganizationUser.objects.filter(user = user, organization__name = workspace['workspace_name']):
+			return "<h2>You are not Authorised to work on this Workspace</h2>" 
+		return f(*args, **kwargs)
+	return decorated_function
 
