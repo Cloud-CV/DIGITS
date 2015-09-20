afa916c67271ec0c98659435fc8b9c193a0ccea2
Ubuntu Ahmed Osman <ahmed.osman@gmail.com>
2015-09-20 05:58:26 +0300

    Fixed a bug that showed all datasets to a user within a workspace

--- digits/model/images/classification/views.py
+++ digits/model/images/classification/views.py
@@ -40,7 +40,7 @@ def image_classification_model_new():
     """
     workspace = get_workspace(flask.request.url)
     form = ImageClassificationModelForm()
-    form.dataset.choices = get_datasets()
+    form.dataset.choices = get_datasets_workspace(workspace)
     form.standard_networks.choices = get_standard_networks()
     form.standard_networks.default = get_default_standard_network()
     form.previous_networks.choices = get_previous_networks()
@@ -582,6 +582,16 @@ def image_classification_model_top_n():
 def get_datasets():
     return [(j.id(), j.name()) for j in sorted(
         [j for j in scheduler.jobs if isinstance(j, ImageClassificationDatasetJob) and (j.status.is_running() or j.status == Status.DONE)],
+        cmp=lambda x,y: cmp(y.id(), x.id())
+        )
+        ]
+def get_datasets_workspace(workspace):
+        list_jobs =   scheduler.return_workspace_jobs(workspace)
+        for j in list_jobs:
+            print j.id()  , j.name()
+        
+        return [(j.id(), j.name()) for j in sorted(
+        [j for j in scheduler.return_workspace_jobs(workspace) if isinstance(j, ImageClassificationDatasetJob) and (j.status.is_running() or j.status == Status.DONE)],
         cmp=lambda x,y: cmp(y.id(), x.id())
         )
         ]
